705882d37e0a7b50d615658974b51245001028bf
Rafael Viana <rafael.lucas111215@gmail.com>
2026-09-02 15:52:31 -0300

    chore: inicialização do projeto Spring Boot

---
+++ src/main/java/br/com/demandwise/DemandwiseApplication.java
@@ -0,0 +1,13 @@
+package br.com.demandwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/demandwise/DemandwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demandwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
